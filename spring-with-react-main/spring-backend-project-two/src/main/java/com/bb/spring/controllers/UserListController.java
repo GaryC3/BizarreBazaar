6d3e672b1e674cec7a4152ec5d05e4bf07ffe77b
100sung <38180151+100sung@users.noreply.github.com>
2022-08-01 08:10:18 -0700

    post on userList

--- spring-with-react-main/spring-backend-project-two/src/main/java/com/bb/spring/controllers/UserListController.java
+++ spring-with-react-main/spring-backend-project-two/src/main/java/com/bb/spring/controllers/UserListController.java
@@ -3,8 +3,13 @@ package com.bb.spring.controllers;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,6 +27,12 @@ public class UserListController {
 	@GetMapping
 	public List<UserList> getUserList(){
 		return userListRepo.findAll();
+	}
+	
+	@PostMapping
+	@Transactional
+	public ResponseEntity<UserList> save(@RequestBody UserList userList){
+		return new ResponseEntity<>(userListRepo.save(userList), HttpStatus.CREATED);
 	}
 
 }
