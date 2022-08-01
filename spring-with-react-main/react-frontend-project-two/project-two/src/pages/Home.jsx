6bc3f6961028d465508ef04e4fa4d1dc1d79fda4
GaryC3 <garychambers3@yahoo.com>
2022-08-01 13:57:43 -0500

    push

--- spring-with-react-main/react-frontend-project-two/project-two/src/pages/Home.jsx
+++ spring-with-react-main/react-frontend-project-two/project-two/src/pages/Home.jsx
@@ -2,8 +2,13 @@
 export const Home = () => {
     return(
         <>
-        
-        <h1>2222222222222221</h1>
+        <h1>The Bizarre Bazaar</h1>
+        <div>
+        <a href="/merchant" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Create</a>
+        <a href="/merchant" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Owned</a>
+        </div>
+        <a href="/merchant" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Browse</a>
+        <a href="/merchant" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Manage</a>
         </>
     );
 }
