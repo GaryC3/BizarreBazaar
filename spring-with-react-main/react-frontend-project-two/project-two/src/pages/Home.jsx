5629b7bfcfe8f47f137b359693d54ea281978443
GaryC3 <garychambers3@yahoo.com>
2022-08-01 15:00:16 -0500

    Home page done
    Without styling

--- spring-with-react-main/react-frontend-project-two/project-two/src/pages/Home.jsx
+++ spring-with-react-main/react-frontend-project-two/project-two/src/pages/Home.jsx
@@ -5,12 +5,16 @@ export const Home = () => {
         <div >
             <h1 class="text-center" style={{fontSize: "40px"}}>The Bizarre Bazaar</h1>
         </div>
-        <div>
-        <a href="/merchant" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Create</a>
-        <a href="/merchant" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Owned</a>
+        <div class="text-center pt-2 pb-5">
+            <a href="/merchant" class="btn btn-primary btn-lg active m-5 py-2 px-5" role="button" aria-pressed="true" style={{fontSize: "40px"}}>Create</a>
+            <a href="/merchant" class="btn btn-primary btn-lg active m-5 py-2 px-5" role="button" aria-pressed="true" style={{fontSize: "40px"}}>Owned</a>
         </div>
-        <a href="/merchant" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Browse</a>
-        <a href="/merchant" class="btn btn-primary btn-lg active" role="button" aria-pressed="true">Manage</a>
+        <div class="text-center pb-5">
+            <a href="/merchant" class="btn btn-primary btn-lg active m-5 p-5" role="button" aria-pressed="true" style={{fontSize: "40px"}}>Browse</a>
+        </div>
+        <div class="text-center">
+        <a href="/merchant" class="btn btn-primary btn-lg active m-5 p-5" role="button" aria-pressed="true" style={{fontSize: "40px"}}>Manage</a>
+        </div>
         </>
     );
 }
