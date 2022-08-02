c972fba5b3fa68cf0270c2a562754b6ae483dc24
GaryC3 <garychambers3@yahoo.com>
2022-08-01 20:17:32 -0500

    fixed importing error in home

--- spring-with-react-main/react-frontend-project-two/project-two/src/pages/Home.jsx
+++ spring-with-react-main/react-frontend-project-two/project-two/src/pages/Home.jsx
@@ -1,11 +1,11 @@
-import {go} from "./components"
+import {go} from "../components"
 export const Home = () => {
     return(
         <>
         <div >
             <h1 class="text-center" style={{fontSize: "40px"}}>The Bizarre Bazaar</h1>
         </div>
-        <go />
+        <go.HNavigate />
         </>
     );
 }
