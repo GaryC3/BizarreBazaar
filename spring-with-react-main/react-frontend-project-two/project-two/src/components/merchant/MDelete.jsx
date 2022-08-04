a6a44b535b06ada0ab930f13dc3f2ddb17d61ac3
100sung <38180151+100sung@users.noreply.github.com>
2022-08-04 14:06:14 -0700

    merchant page  badplaceholder

---
+++ spring-with-react-main/react-frontend-project-two/project-two/src/components/merchant/MDelete.jsx
@@ -0,0 +1,43 @@
+import axios from "axios";
+import { useRef } from 'react';
+
+export const MDelete = () => {
+
+    const title = useRef();
+    const quantity = useRef();
+    return(
+        <>
+        <div class="input-group mb-3 long">
+            <select class="form-control bg-light" searchable="Search here..">
+                <option value="" disabled selected>Select Game</option>
+                <option value="1">a</option>
+                <option value="2">b</option>
+                <option value="3">c</option>
+                <option value="4">d</option>
+                <option value="5">e</option>
+            </select>
+            <input type="number" class="form-control bg-light" placeholder="Quantity" ara-label="quantity" required />
+            
+        </div>
+        <div class= "input-group mb-3 long3">
+            <button class="btn btn-primary active button1" type="button" /*onClick={handleSubmit}*/>Delete</button>
+
+            <button class="btn btn-primary active button1" type="button" /*onClick={handleSubmit}*/>Update</button>
+        </div>
+        
+       
+        
+            
+                
+                    
+                    {/* <input type= "textarea" class="bg-light bigtextbox" placeholder="Description" aria-label="description" /> */}
+                    {/* <button class="btn btn-primary active merchantaddsubmit" type="button" /*onClick={handleSubmit}*//*>Submit</button> */}
+                    
+                
+        
+        
+        </>   
+        
+        
+    );
+}
--- spring-with-react-main/react-frontend-project-two/project-two/src/pages/Merchant.jsx
+++ spring-with-react-main/react-frontend-project-two/project-two/src/pages/Merchant.jsx
@@ -1,8 +1,16 @@
+import {admin} from "../components"
 export const Merchant = () => {
     return(
         <>
+        <div >
+            <h1 class="text-center title">Add new game</h1>
+            <admin.MNew />
+            <h1 class="text-center title">Update/Delete</h1>
+            <admin.MDelete />
+            <h1 class="text-center title">Update discounts</h1>
+            <admin.MDiscount />
+        </div>  
         
-        <h1>4</h1>
         </>
     );
 }
