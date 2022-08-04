6edf52adf96f2f7fe9c63e5ca61bb3478804f5ff
GaryC3 <garychambers3@yahoo.com>
2022-08-04 15:49:02 -0500

    back button done
    easy to add to any page

---
+++ spring-with-react-main/react-frontend-project-two/project-two/src/components/Back.jsx
@@ -0,0 +1,7 @@
+export const Back = () => {
+    return(
+        <div>
+            <a href="/" className="btn btn-primary btn-lg active back" role="button" >←</a>
+        </div>
+    ); 
+}
--- spring-with-react-main/react-frontend-project-two/project-two/src/components/SSigning.jsx
+++ spring-with-react-main/react-frontend-project-two/project-two/src/components/SSigning.jsx
@@ -36,21 +36,21 @@ export const SSigning = () => {
 
     return(
         <>
-        <div class="input-group mb-3 long">
-            <input type="text" class="form-control bg-light" placeholder="First Name" aria-label="firstName" /*id="firstName" ref={firstRef}*/ />
-            <input type="text" class="form-control bg-light" placeholder="Last Name" aria-label="LastName" /*id="lastName" ref={lastRef}*/ />
+        <div className="input-group mb-3 long">
+            <input type="text" className="form-control bg-light" placeholder="First Name" aria-label="firstName" /*id="firstName" ref={firstRef}*/ />
+            <input type="text" className="form-control bg-light" placeholder="Last Name" aria-label="LastName" /*id="lastName" ref={lastRef}*/ />
         </div>
-        <div class="input-group mb-3">
+        <div className="input-group mb-3">
             <div>
                 <div>
-                    <input type="text" class="form-control bg-light long2" placeholder="Email" aria-label="Email" /*id="email" ref={emailRef}*/ />
+                    <input type="text" className="form-control bg-light long2" placeholder="Email" aria-label="Email" /*id="email" ref={emailRef}*/ />
                 </div>
                 <div>
-                    <input type="password" class="form-control bg-light long2" placeholder="Password" aria-label="Password" /*id="password" ref={passwordRef}*/ />
+                    <input type="password" className="form-control bg-light long2" placeholder="Password" aria-label="Password" /*id="password" ref={passwordRef}*/ />
                 </div>
             </div>
             <div>
-                <button class="btn btn-primary active button1" type="button" /*onClick={handleSubmit}*/>Submit</button>
+                <button className="btn btn-primary active button1" type="button" /*onClick={handleSubmit}*/>Submit</button>
             </div>
         </div>
         </> 
