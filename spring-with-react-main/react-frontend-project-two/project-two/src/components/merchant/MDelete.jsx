59d1c5f561745712ec6668beeff5dc9cec1c3839
100sung <38180151+100sung@users.noreply.github.com>
2022-08-05 12:55:19 -0700

    more mdelete things

--- spring-with-react-main/react-frontend-project-two/project-two/src/components/merchant/MDelete.jsx
+++ spring-with-react-main/react-frontend-project-two/project-two/src/components/merchant/MDelete.jsx
@@ -7,43 +7,60 @@ export const MDelete = () => {
     const quantity = useRef();
 
     
-    const[setGames] = useState([]);
+    const[games, setGames] = useState([]);
 
         useEffect(() => {  
         axios.get('http://localhost:8080/gameList')
             .then(res => {setGames(res.data)}) // sets my devs state to be that array of devs
         }, []); 
+
+        const handleDelete = async (e) =>{
+            try{
+                e.preventDefault();
+                await axios.delete(`http://localhost:8080/gameList?gameid}`, {
+                    // first_name: firstRef.current.value,
+                    // last_name: lastRef.current.value,
+                    // email: emailRef.current.value,
+                    // password: passwordRef.current.value
+                });
+            }catch(err){
+                console.log(err); 
+            }
+        }
+        const handleUpdate = async (e) =>{
+            try{
+                e.preventDefault();
+                await axios.put(`http://localhost:8080/gameList}`, {
+                    // first_name: firstRef.current.value,
+                    // last_name: lastRef.current.value,
+                    // email: emailRef.current.value,
+                    // password: passwordRef.current.value
+                });
+            }catch(err){
+                console.log(err); 
+            }
+        }
     return(
         <>
+        
         <div class="input-group mb-3 long">
-            <select class="form-control bg-light" searchable="Search here..">
-                <option value="" disabled selected>Select Game</option>
-                <option value="1">a</option>
-                <option value="2">b</option>
-                <option value="3">c</option>
-                <option value="4">d</option>
-                <option value="5">e</option>
-            </select>
-            <input type="number" class="form-control bg-light" placeholder="Quantity" ara-label="quantity" required />
+        <select class="form-control bg-light" searchable="Search here..">
+        <option value="" disabled selected>Select Game</option>
+        {games.map((game, i, e) =>{
+                    return(
+                         <option value="game">{game.title} </option> 
+                    )
+                }
+                )}
+            </select> 
+            <input type="number" class="form-control bg-light" placeholder="quantity" ara-label="quantity" required />
             
         </div>
         <div class= "input-group mb-3 long3">
-            <button class="btn btn-primary active button1" type="button" /*onClick={handleSubmit}*/>Delete</button>
+            <button class="btn btn-primary active button1" type="button" /*onClick={handleDelete}*/>Delete</button>
 
-            <button class="btn btn-primary active button1" type="button" /*onClick={handleSubmit}*/>Update</button>
-        </div>
-        
-       
-        
-            
-                
-                    
-                    {/* <input type= "textarea" class="bg-light bigtextbox" placeholder="Description" aria-label="description" /> */}
-                    {/* <button class="btn btn-primary active merchantaddsubmit" type="button" /*onClick={handleSubmit}*//*>Submit</button> */}
-                    
-                
-        
-        
+            <button class="btn btn-primary active button1" type="button" /*onClick={handleUpdate}*/>Update</button>
+        </div>    
         </>   
         
         
