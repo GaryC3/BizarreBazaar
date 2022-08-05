816415b1d0917ebcb4c10930551325d8a2335c95
GaryC3 <garychambers3@yahoo.com>
2022-08-05 13:46:39 -0500

    Browse Games done minus ability to store what you chose on purchase

--- spring-with-react-main/react-frontend-project-two/project-two/src/components/OGames.jsx
+++ spring-with-react-main/react-frontend-project-two/project-two/src/components/OGames.jsx
@@ -3,6 +3,7 @@ import { useState, useEffect } from 'react';
 
 export const OGames = () => {
     const[games, setGames] = useState([]);
+    
 
         useEffect(() => {  
         axios.get('http://localhost:8080/gameList')
@@ -13,23 +14,11 @@ export const OGames = () => {
     return(
         <> 
             <tbody>
-                <div class="container-fluid">
-                    <div class="row">
                         {games.map((game) =>{
                             return(
-                                //<SoftwareDeveloper key={dev.id} dev={dev} developers={devs} setDevs={setDevs} />
-                                // <div class="container-fluid">
-                                     <div class="row">
-                                        <img class="gameSize" src="placeholder.png" alt="game" />
-                                        {/* <div class="caption">
-                                            {game.title}
-                                        </div> */}
-                                     {/* </div>    */}
-                                </div> 
+                                <img class="gameSize" src="placeholder.png" alt="game" />
                             );
                         })}
-                    </div>   
-                </div>
             </tbody> 
         </> 
     );
--- spring-with-react-main/react-frontend-project-two/project-two/src/components/purchase/PGames.jsx
+++ spring-with-react-main/react-frontend-project-two/project-two/src/components/purchase/PGames.jsx
@@ -12,24 +12,93 @@ export const PGames = () => {
 
     return(
         <> 
-            <tbody>
-                <div class="container-fluid">
-                    <div class="row">
-                        {games.map((game) =>{
-                            return(
-                                //<SoftwareDeveloper key={dev.id} dev={dev} developers={devs} setDevs={setDevs} />
-                                // <div class="container-fluid">
-                                     <div class="row">
-                                        <img class="gameSize" src="placeholder.png" alt="game" />
-                                        {/* <div class="caption">
-                                            {game.title}
-                                        </div> */}
-                                     {/* </div>    */}
-                                </div> 
-                            );
-                        })}
-                    </div>   
-                </div>
+            <tbody id ="container">
+                {games.map((game, i, e) =>{
+                    //i *=3 ;
+                    //var temp = e[i+1];
+                    return(
+                        // <div>
+                        //     <div> 
+                        //         <img class="gameSize" src="placeholder.png" alt="game" />
+                        //         <img class="gameSize" src="placeholder.png" alt="game" />
+                        //         <img class="gameSize" src="placeholder.png" alt="game" />
+                        //     </div>  
+                        //     <div key={i}>
+                        //         {}
+                        //         <div class="caption">
+                        //             {{i}.title}
+                        //         </div>
+                        //         <div class="caption">
+                        //             {game.title}
+                        //         </div>
+                        //         <div class="caption">
+                        //             {i}
+                        //         </div>
+                        //     </div>
+                        // </div>
+                        
+                        <div>
+                            <div>
+                                <div>
+                                    <img class="gameSize" src="placeholder.png" alt="game" />
+                                    <input type="checkbox" id="exampleCheck1"/>
+                                    <div class="list-group">
+                                        <a class="text-center">Title :</a>
+                                        <a class=" list-group-item-action list-group-item-secondary text-center">{game.title}</a>
+                                    </div>
+                                    <div class="list-group">
+                                        <a class="text-center">Price :</a>
+                                        <a class=" list-group-item-action list-group-item-secondary text-center">${Math.ceil((game.price-game.price*game.discount.discount)*100)/100}</a>
+                                    </div>
+                                </div>
+                                {/* <div>
+                                    <img class="gameSize" src="placeholder.png" alt="game" />
+                                    <input type="checkbox" id="exampleCheck1"/>
+                                    <div class="list-group">
+                                        <a class="text-center">Title:</a>
+                                        <a class=" list-group-item-action list-group-item-secondary text-center">{game.title}</a>
+                                    </div>
+                                    <div class="list-group">
+                                        <a class="text-center">Price:</a>
+                                        <a class=" list-group-item-action list-group-item-secondary text-center">{game.price}</a>
+                                    </div>
+                                </div>
+                                <div>
+                                    <img class="gameSize" src="placeholder.png" alt="game" />
+                                    <input type="checkbox" id="exampleCheck1"/>
+                                    <div class="list-group">
+                                        <a class="text-center">Title:</a>
+                                        <a class=" list-group-item-action list-group-item-secondary text-center">{game.title}</a>
+                                    </div>
+                                    <div class="list-group">
+                                        <a class="text-center">Price:</a>
+                                        <a class=" list-group-item-action list-group-item-secondary text-center">{game.price}</a>
+                                    </div>
+                                </div> */}
+
+
+
+                                    {/* <div class="container-fluid">
+
+                                        <div class="container panel panel-default">
+
+                                            <ul class="list-group">
+                                                <li class="row list-group-item">
+                                                <a class="col-sm-2" ng-href="#">Name</a> 
+                                                <a class="col-sm-2" ng-href="#">Tweak Roster</a>
+                                                <a class="col-sm-2" ng-href="#">This Week's Matchup</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+                                    </div> */}
+                            </div>
+                        </div>
+
+                        
+
+
+                    );
+                })}
             </tbody> 
         </> 
     );
