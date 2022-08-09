0ef112286f90cc763d19c9747d64111eddfdac0c
GaryC3 <garychambers3@yahoo.com>
2022-08-09 10:02:37 -0500

    clean-up for delete

--- spring-with-react-main/spring-backend-project-two/src/main/java/com/bb/spring/controllers/InvoiceLinesController.java
+++ spring-with-react-main/spring-backend-project-two/src/main/java/com/bb/spring/controllers/InvoiceLinesController.java
@@ -7,16 +7,16 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.bb.spring.beans.GameList;
 import com.bb.spring.beans.Invoice;
 import com.bb.spring.beans.InvoiceLines;
-import com.bb.spring.repositories.GameListRepo;
 import com.bb.spring.repositories.InvoiceLineRepo;
 import com.bb.spring.repositories.InvoiceRepo;
 import com.fasterxml.jackson.databind.node.ObjectNode;
@@ -32,13 +32,7 @@ public class InvoiceLinesController {
 	@Autowired
 	private InvoiceRepo invoiceRepo;
 	
-	@Autowired
-	private GameListRepo gameListRepo;
-	
 	@GetMapping
-//	public List<GameList> getGameList(){
-//		return gameListRepo.findAll();
-//	}
 	public List<Invoice> getInvoice(@RequestBody ObjectNode node){
 		int userid = node.get("id").asInt();
 		 return invoiceRepo.findByUserId(userid);
@@ -49,5 +43,11 @@ public class InvoiceLinesController {
 	public ResponseEntity<InvoiceLines> save(@RequestBody InvoiceLines invoiceLine){
 		return new ResponseEntity<>(invoiceLineRepo.save(invoiceLine), HttpStatus.CREATED);
 	}
-
+	
+	@DeleteMapping("/{userid}")
+	public ResponseEntity<InvoiceLines> delete(@PathVariable String userid){
+		int realuserid = Integer.parseInt(userid);
+		invoiceLineRepo.deleteByUserId(realuserid);
+		return ResponseEntity.noContent().build();
+	}
 }
