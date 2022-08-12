e028e5d9a747ca5a5727d1b2f84a515549569aff
100sung <38180151+100sung@users.noreply.github.com>
2022-08-12 06:11:50 -0700

    update, filter implementation in progress

--- spring-with-react-main/spring-backend-project-two/src/main/java/com/bb/spring/SpringSecurityConfig.java
+++ spring-with-react-main/spring-backend-project-two/src/main/java/com/bb/spring/SpringSecurityConfig.java
@@ -1,11 +1,13 @@
 package com.bb.spring;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.sql.DataSource;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,7 +18,9 @@ import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+import com.bb.spring.jwt.JwtTokenFilter;
 import com.bb.spring.repositories.UserListRepo;
 
 @Configuration
@@ -25,6 +29,8 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 
 	@Autowired
 	private UserListRepo userListRepo;
+	@Autowired
+	private JwtTokenFilter jwtTokenFilter;
 	
 	@Bean
 	public PasswordEncoder passwordEncoder() {
@@ -51,12 +57,16 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
 	protected void configure(HttpSecurity http) throws Exception {
 		http.csrf().disable().httpBasic(); // not-prod
 		http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//		http.authorizeRequests().mvcMatchers("/login-check").hasAnyRole("USER");
-//		http.authorizeRequests().mvcMatchers("/artist/**").hasAnyRole("USER");
-//		http.authorizeRequests().mvcMatchers("/album/**").hasAnyRole("ADMIN");
-//		http.authorizeRequests().mvcMatchers("/tracks/**").hasAnyRole("ADMIN");
-		http.authorizeRequests().mvcMatchers("/**").permitAll();
-		http.authorizeRequests().anyRequest().permitAll();
+		http.exceptionHandling().authenticationEntryPoint(
+				(request, response, ex) -> {
+					response.sendError(HttpServletResponse.SC_UNAUTHORIZED,
+					ex.getMessage());
+					}
+				);
+		http.authorizeRequests().mvcMatchers("/**").permitAll(); // if this is placed below the jwttoken filter, it will not COMPILE
+		http.authorizeRequests().antMatchers("/auth/login").permitAll().anyRequest().authenticated();
+		http.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+
 		http.logout().deleteCookies("custom-cookie").invalidateHttpSession(false); // POST /logout
 		// CSRF - 
 		// 1. login to Bank of America (session, cookie)
