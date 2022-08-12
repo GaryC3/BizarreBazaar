0e924b9f4d07a4ac61fd6f4d4dc3858492a1f980
GaryC3 <garychambers3@yahoo.com>
2022-08-12 08:35:23 -0500

    wfed
    a

---
+++ spring-with-react-main/spring-backend-project-two/src/main/java/com/bb/spring/SecurityConfig.java
@@ -0,0 +1,53 @@
+package com.bb.spring;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+//@EnableGlobalMethodSecurity  // role-based access control (RBAC) to methods.. not just URL
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+	@Autowired // Spring Data JPA should have a datasource already
+	private DataSource datasource;
+	
+	@Autowired
+	private PasswordEncoder passwordEncoder;
+	
+	// Spring Security Filter logic (pseudo)
+	// 1. http-req Authentication Header, filter extract header/ decode base64 Authentication header
+	// 2. hashes password with Bcrypt -  $2a$10$iCyW/yDkb6URKSeVwKrQt.FoIf5TJs87OkZfShVWuMQJWSHG83nGe
+	// 3. jdbc auth, select username, password, enabled from users where username = ?
+	// 4. !enabled? 401 : next
+	// 5. $2a$10$iCyW/yDkb6URKSeVwKrQt.FoIf5TJs87OkZfShVWuMQJWSHG83nGe check with the password value from DB
+	// 6. authenticated or not?
+	// 7. Principal (user currently logged in). stores in HttpSession (any data needs to be Serializable) 
+	
+	@Autowired
+	protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+		auth.jdbcAuthentication().dataSource(datasource).passwordEncoder(passwordEncoder);
+	}
+	
+	protected void configure(HttpSecurity http) throws Exception {
+		http.csrf().disable().httpBasic(); // not-prod
+		http.authorizeRequests().mvcMatchers("/").hasAnyRole("USER");
+		http.authorizeRequests().mvcMatchers("/sign").hasAnyRole("USER");
+		http.authorizeRequests().mvcMatchers("/browse").hasAnyRole("ADMIN");
+		http.authorizeRequests().mvcMatchers("/owned").hasAnyRole("ADMIN");
+		http.logout().deleteCookies("custom-cookie").invalidateHttpSession(false); // POST /logout
+		// CSRF - 
+		// 1. login to Bank of America (session, cookie)
+		// 2. hacker send you an email with link
+		// 3. click link href=malicious JS   POST boa.com/api/transfer/your-acct/myacct/balance
+		// 4. YOU initiated the request
+		
+		// generate a random csrf_token. Unsafe methods MUST contain the CSRF token or-else request is denied
+		
+	}
+	
+}
--- spring-with-react-main/spring-backend-project-two/src/main/java/com/bb/spring/SpringBackendProjectTwoApplication.java
+++ spring-with-react-main/spring-backend-project-two/src/main/java/com/bb/spring/SpringBackendProjectTwoApplication.java
@@ -3,17 +3,30 @@ package com.bb.spring;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
-import com.bb.spring.beans.User;
 import com.bb.spring.repositories.UserRepository;
 
 @SpringBootApplication
+@ComponentScan("com.bb.spring")
+@Configuration
+@EnableAutoConfiguration
 public class SpringBackendProjectTwoApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		// Application Context
 		SpringApplication.run(SpringBackendProjectTwoApplication.class, args);
+	}
+	
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder(); //10 salt
 	}
 
 	@Autowired
