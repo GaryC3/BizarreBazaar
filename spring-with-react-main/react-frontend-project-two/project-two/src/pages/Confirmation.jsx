11e6b22e82291d1412905976e1fbf4345e7238a2
100sung <38180151+100sung@users.noreply.github.com>
2022-08-14 14:06:02 -0700

    backgroundsdone

--- spring-with-react-main/react-frontend-project-two/project-two/src/pages/Confirmation.jsx
+++ spring-with-react-main/react-frontend-project-two/project-two/src/pages/Confirmation.jsx
@@ -3,9 +3,11 @@ import {cart} from "../components"
 export const Confirmation = () => {
     return(
         <>
+        <div className = "conBackground">
         <back.Back/>
         <h1 className="text-center title">CART</h1>
         <cart.CGames/>
+        </div>
         </>
     );
 }
