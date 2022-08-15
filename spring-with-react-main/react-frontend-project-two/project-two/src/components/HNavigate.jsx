4cc53f4e21cee57ce468fbf8288e6a7a6a20a9df
GaryC3 <garychambers3@yahoo.com>
2022-08-14 19:50:34 -0500

    Made it so buttons get disabled when not logged in

--- spring-with-react-main/react-frontend-project-two/project-two/src/components/HNavigate.jsx
+++ spring-with-react-main/react-frontend-project-two/project-two/src/components/HNavigate.jsx
@@ -1,16 +1,24 @@
 export const HNavigate = () => {
+    var temp;
+    console.log(localStorage.getItem("id"))
+    if (localStorage.getItem("id")){
+        temp = "btn btn-primary btn-lg active m-5 py-3 px-5"
+    }
+    else{
+        temp = "disabled btn btn-primary btn-lg active m-5 py-3 px-5"
+    }
     return(
         <>
         <div class="text-center pt-2 pb-5">
-            <a href="/sign" class="btn btn-primary btn-lg active m-5 py-3 px-5" role="button" aria-pressed="true" style={{fontSize: "40px"}}>Create</a>
+            <a href="/sign" class="btn btn-primary btn-lg active m-5 py-3 px-5" role="button" aria-pressed="true" style={{fontSize: "40px"} }>Create</a>
             <a href="/sign2" class="btn btn-primary btn-lg active m-5 py-3 px-5" role="button" aria-pressed="true" style={{fontSize: "40px"}}>Sign-In</a>
         </div>
         <div class="text-center pb-5">
-            <a href="/purchase" class="btn btn-primary btn-lg active m-5 py-3 px-5" role="button" aria-pressed="true" style={{fontSize: "40px"}}>Browse</a>
-            <a href="/owned" class="btn btn-primary btn-lg active m-5 py-3 px-5" role="button" aria-pressed="true" style={{fontSize: "40px"}}>Owned</a>
+            <a href="/purchase" class={temp} role="button" aria-pressed="true" style={{fontSize: "40px"}}>Browse</a>
+            <a href="/owned" class={temp} role="button" aria-pressed="true" style={{fontSize: "40px"}}>Owned</a>
         </div>
         <div class="text-center">
-        <a href="/merchant" class="btn btn-primary btn-lg active m-5 p-5" role="button" aria-pressed="true" style={{fontSize: "40px"}}>Manage</a>
+        <a href="/merchant" class={temp} role="button" aria-pressed="true" style={{fontSize: "40px"}}>Manage</a>
         </div>
         </> 
     ); 
